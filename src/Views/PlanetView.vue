064ec6537f201be9cdd75090b866300952ff784f
RyuzakL <logan.benard7@icloud.com>
2023-02-17 21:27:42 +0100

    les images de planets changent quand on sélectionne une autra tab

--- src/Views/PlanetView.vue
+++ src/Views/PlanetView.vue
@@ -7,10 +7,11 @@ import PlanetDetails from '@/components/PlanetDetails.vue'
 import Navbar from '@/components/Navbar.vue'
 import { ref, computed } from 'vue';
 
-const selectedPlanet = ref('mercury')
+const selectedPlanet = ref('neptune')
 const currentPlanet = computed(() => planetsJSON[selectedPlanet.value])
 const currentTab = ref('overview')
 const updateTab = (newTab) => currentTab.value = newTab
+const isSurfaceTab = () => currentTab.value === 'surface'
 console.log(currentPlanet.value)
 
 </script>
@@ -20,7 +21,12 @@ console.log(currentPlanet.value)
 	<Navbar :planet="currentPlanet" @set-current-tab="updateTab" />
 	<hr>
 	<Container class="grid place-items-center">
-		<img class="w-[35%] my-24" :src="`src/${currentPlanet[currentTab].planet}`" alt="">
+		<div class="w-[100%] grid place-items-center relative my-24">
+			<img :class="{ 'absolute w-[19%] top-[70px]': isSurfaceTab() }" class="w-[35%] z-10"
+				:src="`src/${currentPlanet[currentTab].planet}`" alt="planet image ">
+			<img v-if="isSurfaceTab()" class="w-[35%] z-0" :src="`src/${currentPlanet.overview.planet}`"
+				alt="planet image detail">
+		</div>
 		<h2 class="tracking-normal mb-4">{{ (currentPlanet.name).toUpperCase() }}</h2>
 		<p class="text-center tracking-normal leading-5 mb-8 ">{{ currentPlanet[currentTab].content }}</p>
 		<span class="flex items-center gap-2 text-greyLight mb-6">Source : <a class="text-greyLight font-bold underline"
@@ -29,6 +35,6 @@ console.log(currentPlanet.value)
 		<PlanetDetails :label="'revolution time'" :details="currentPlanet.revolution" />
 		<PlanetDetails :label="'radius'" :details="currentPlanet.radius" />
 		<PlanetDetails :label="'average temp.'" :details="currentPlanet.temperature" />
-	</Container>
+</Container>
 </template>	
 
